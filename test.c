8082ca4ab8be2d736dabf828f902fd4c383cb616
NateLoken <nfloken@comcast.net>
2019-10-03 21:44:13 -0600

    changed the test to be more like the example

--- test.c
+++ test.c
@@ -1,4 +1,10 @@
-main(){
-    puts("Hello World!");
+func(){
+    puts("hello world");
     puts("this is a test");
+}
+
+main(){
+    func("goodbye", "second", 42);
+    printf("printf call %s %d\n", "and more", 42 + 4 + 5 + 2);
+    puts("hello world");
 }
