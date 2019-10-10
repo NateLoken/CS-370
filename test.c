191e579a5a304a3d39001baead097d41a29011bc
NateLoken <nfloken@comcast.net>
2019-10-09 23:37:27 -0600

    added single numbers to the function arguments

--- test.c
+++ test.c
@@ -4,7 +4,7 @@ func(){
 }
 
 main(){
-    func("goodbye", "second");
-    printf("printf call %s \n", "and more");
+    func("goodbye", "second", 42);
+    printf("printf call %s %d\n", "and more", 42);
     puts("hello world");
 }
