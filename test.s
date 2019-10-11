b5d69cb9f46b435d8cf6fae3c609f2cf4335d4f0
NateLoken <nfloken@comcast.net>
2019-10-10 23:31:48 -0600

    assembly code now assembles to make the correct a.out for the test code

--- test.s
+++ test.s
@@ -2,8 +2,6 @@
 	.section	.rodata
 .LC0:
 	.string	"hello world"
-.LC1:
-	.string	"this is a test"
 	.text
 	.globl	func
 	.type	func, @function
@@ -18,9 +16,6 @@ func:
 	mov	$.LC0, %rdi
 	movl	$0, %eax
 	call	puts
-	mov	$.LC1, %rdi
-	movl	$0, %eax
-	call	puts
 	popq	%rbp
 	.cfi_def_cfa	7, 8
 	ret
@@ -28,15 +23,15 @@ func:
 .LFE0:
 	.size	func, .-func
 
-.LC2:
+.LC1:
 	.string	"goodbye"
-.LC3:
+.LC2:
 	.string	"second"
-.LC4:
+.LC3:
 	.string	"printf call %s %d\n"
-.LC5:
+.LC4:
 	.string	"and more"
-.LC6:
+.LC5:
 	.string	"hello world"
 	.text
 	.globl	main
@@ -49,17 +44,23 @@ main:
 	.cfi_offset	6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register	6
-	mov	$.LC2, %rdi
-	mov	$.LC3, %rsi
-	movl	$42, %edx
+	mov	$.LC1, %rdi
+	mov	$.LC2, %rsi
+	mov	$42, %rdx
 	movl	$0, %eax
 	call	func
-	mov	$.LC4, %rdi
-	mov	$.LC5, %rsi
-	movl	$42, %edx
+	mov	$.LC3, %rdi
+	mov	$.LC4, %rsi
+	mov	$42, %rdx
+	mov	$4, %rcx
+	add	%rcx, %rdx
+	mov	$5, %r8
+	add	%r8, %rdx
+	mov	$2, %r9
+	add	%r9, %rdx
 	movl	$0, %eax
 	call	printf
-	mov	$.LC6, %rdi
+	mov	$.LC5, %rdi
 	movl	$0, %eax
 	call	puts
 	popq	%rbp
