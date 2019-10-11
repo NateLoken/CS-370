09bb3e804e78421f3fc50ea94da99bd8f562ad6f
NateLoken <nfloken@comcast.net>
2019-10-10 23:30:48 -0600

    test code now fits the example from the lab

--- test.c
+++ test.c
@@ -1,10 +1,9 @@
 func(){
     puts("hello world");
-    puts("this is a test");
 }
 
 main(){
     func("goodbye", "second", 42);
-    printf("printf call %s %d\n", "and more", 42);
+    printf("printf call %s %d\n", "and more", 42+4+5+2);
     puts("hello world");
 }
